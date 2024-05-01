5920bf2c109afd9575c432af7bea15ef98b5b3d9
yaswanthk63 <155880681+yaswanthk63@users.noreply.github.com>
2024-05-01 14:12:00 +0530

    Add files via upload

---
+++ Resume builder/Resume builder/assets/js/app.js
@@ -0,0 +1,238 @@
+// regex for validation
+const strRegex =  /^[a-zA-Z\s]*$/; // containing only letters
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const phoneRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+/* supports following number formats - (123) 456-7890, (123)456-7890, 123-456-7890, 123.456.7890, 1234567890, +31636363634, 075-63546725 */
+const digitRegex = /^\d+$/;
+
+const mainForm = document.getElementById('cv-form');
+const validType = {
+    TEXT: 'text',
+    TEXT_EMP: 'text_emp',
+    EMAIL: 'email',
+    DIGIT: 'digit',
+    PHONENO: 'phoneno',
+    ANY: 'any',
+}
+
+// user inputs elements
+let firstnameElem = mainForm.firstname,
+    middlenameElem = mainForm.middlename,
+    lastnameElem = mainForm.lastname,
+    imageElem = mainForm.image,
+    addressElem = mainForm.address,
+    emailElem = mainForm.email,
+    phonenoElem = mainForm.phoneno,
+    linkedin= mainForm.linkedin.value,
+    github = mainForm.github.value,
+    caption = mainForm.caption.value;
+
+// display elements
+let nameDsp = document.getElementById('fullname_dsp'),
+    imageDsp = document.getElementById('image_dsp'),
+    phonenoDsp = document.getElementById('phoneno_dsp'),
+    emailDsp = document.getElementById('email_dsp'),
+    addressDsp = document.getElementById('address_dsp'),
+    skillsDsp = document.getElementById('skills_dsp'),
+    certificatesDsp = document.getElementById('certificates_dsp'),
+    languagesDsp = document.getElementById('languages_dsp'),
+    interestsDsp = document.getElementById('interests_dsp'),
+    educationsDsp = document.getElementById('educations_dsp'),
+    experiencesDsp = document.getElementById('experiences_dsp'),
+    linkedinDsp = document.getElementById('linkedin_dsp'),
+    githubDsp = document.getElementById('github_dsp'),
+    captionDsp = document.getElementById('caption_dsp');
+
+
+// first value is for the attributes and second one passes the nodelists
+const fetchValues = (attrs, ...nodeLists) => {
+    let elemsAttrsCount = nodeLists.length;
+    let elemsDataCount = nodeLists[0].length;
+    let tempDataArr = [];
+
+    // first loop deals with the no of repeaters value
+    for(let i = 0; i < elemsDataCount; i++){
+        let dataObj = {}; // creating an empty object to fill the data
+        // second loop fetches the data for each repeaters value or attributes 
+        for(let j = 0; j < elemsAttrsCount; j++){
+            // setting the key name for the object and fill it with data
+            dataObj[`${attrs[j]}`] = nodeLists[j][i].value;
+        }
+        tempDataArr.push(dataObj);
+    }
+
+    return tempDataArr;
+}
+
+const getUserInputs = () => {
+
+    
+    
+    // experiences
+    let expTitleElem = document.querySelectorAll('.exp_title'),
+    expOrganizationElem = document.querySelectorAll('.exp_organization'),
+    expLocationElem = document.querySelectorAll('.exp_location'),
+    expStartDateElem = document.querySelectorAll('.exp_start_date'),
+    expEndDateElem = document.querySelectorAll('.exp_end_date'),
+    expDescriptionElem = document.querySelectorAll('.exp_description');
+
+    // education
+    let eduSchoolElem = document.querySelectorAll('.edu_school'),
+    eduDegreeElem = document.querySelectorAll('.edu_degree'),
+    eduCityElem = document.querySelectorAll('.edu_city'),
+    eduStartDateElem = document.querySelectorAll('.edu_start_date'),
+    eduGraduationDateElem = document.querySelectorAll('.edu_graduation_date'),
+    eduDescriptionElem = document.querySelectorAll('.edu_description');
+
+    let skillElem = document.querySelectorAll('.skill');
+
+    let certificateElem = document.querySelectorAll('.certificate');
+
+    let languageElem = document.querySelectorAll('.language');
+
+    let interestElem = document.querySelectorAll('.interest');
+
+    // event listeners for form validation
+    firstnameElem.addEventListener('keyup', (e) => validateFormData(e.target, validType.TEXT, 'First Name'));
+    middlenameElem.addEventListener('keyup', (e) => validateFormData(e.target, validType.TEXT_EMP, 'Middle Name'));
+    lastnameElem.addEventListener('keyup', (e) => validateFormData(e.target, validType.TEXT, 'Last Name'));
+    phonenoElem.addEventListener('keyup', (e) => validateFormData(e.target, validType.PHONENO, 'Phone Number'));
+    emailElem.addEventListener('keyup', (e) => validateFormData(e.target, validType.EMAIL, 'Email'));
+    addressElem.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'Address'));
+    mainForm.linkedin.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'LinkedIn'));
+    mainForm.github.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'GitHub'));
+    mainForm.caption.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'Caption'));
+
+
+    expTitleElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'Title')));
+    expOrganizationElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'Organization')));
+    expLocationElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, "Location")));
+    expStartDateElem.forEach(item => item.addEventListener('blur', (e) => validateFormData(e.target, validType.ANY, 'End Date')));
+    expEndDateElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'End Date')));
+    expDescriptionElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'Description')));
+    eduSchoolElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'Education level')));
+    eduDegreeElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'Degree')));
+    eduCityElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'City')));
+    eduStartDateElem.forEach(item => item.addEventListener('blur', (e) => validateFormData(e.target, validType.ANY, 'Start Date')));
+    eduGraduationDateElem.forEach(item => item.addEventListener('blur', (e) => validateFormData(e.target, validType.ANY, 'Graduation Date')));
+    eduDescriptionElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'Description')));
+    skillElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'skill')));
+    certificateElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'Certificate')));
+    languageElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'Language')));
+    interestElem.forEach(item => item.addEventListener('keyup', (e) => validateFormData(e.target, validType.ANY, 'Interest')));
+
+    return {
+        firstname: firstnameElem.value,
+        middlename: middlenameElem.value,
+        lastname: lastnameElem.value,
+        address: addressElem.value,
+        email: emailElem.value,
+        phoneno: phonenoElem.value,
+        linkedin: mainForm.linkedin.value,
+        github: mainForm.github.value,
+        caption: mainForm.caption.value,
+        experiences: fetchValues(['exp_title', 'exp_organization', 'exp_location', 'exp_start_date', 'exp_end_date', 'exp_description'], expTitleElem, expOrganizationElem, expLocationElem, expStartDateElem, expEndDateElem, expDescriptionElem),
+        educations: fetchValues(['edu_school', 'edu_degree', 'edu_city', 'edu_start_date', 'edu_graduation_date', 'edu_description'], eduSchoolElem, eduDegreeElem, eduCityElem, eduStartDateElem, eduGraduationDateElem, eduDescriptionElem),
+        skills: fetchValues(['skill'], skillElem),
+        certificates: fetchValues(['certificate'], certificateElem),
+        languages: fetchValues(['language'], languageElem),
+        interests: fetchValues(['interest'], interestElem)
+
+    }
+};
+
+function validateFormData(elem, elemType, elemName){
+    // checking for text string and non empty string
+    if(elemType == validType.TEXT){
+        if(!strRegex.test(elem.value) || elem.value.trim().length == 0) addErrMsg(elem, elemName);
+        else removeErrMsg(elem);
+    }
+
+    // checking for only text string
+    if(elemType == validType.TEXT_EMP){
+        if(!strRegex.test(elem.value)) addErrMsg(elem, elemName);
+        else removeErrMsg(elem);
+    }
+
+    // checking for email
+    if(elemType == validType.EMAIL){
+        if(!emailRegex.test(elem.value) || elem.value.trim().length == 0) addErrMsg(elem, elemName);
+        else removeErrMsg(elem);
+    }
+
+    // checking for phone number
+    if(elemType == validType.PHONENO){
+        if(!phoneRegex.test(elem.value) || elem.value.trim().length == 0) addErrMsg(elem, elemName);
+        else removeErrMsg(elem);
+    }
+
+    // checking for only empty
+    if(elemType == validType.ANY){
+        if(elem.value.trim().length == 0) addErrMsg(elem, elemName);
+        else removeErrMsg(elem);
+    }
+}
+
+// adding the invalid text
+function addErrMsg(formElem, formElemName){
+    formElem.nextElementSibling.innerHTML = `${formElemName} is invalid`;
+}
+
+// removing the invalid text 
+function removeErrMsg(formElem){
+    formElem.nextElementSibling.innerHTML = "";
+}
+
+// show the list data
+const showListData = (listData, listContainer) => {
+    listContainer.innerHTML = "";
+    listData.forEach(listItem => {
+        let itemElem = document.createElement('div');
+        itemElem.classList.add('preview-item');
+        
+        for(const key in listItem){
+            let subItemElem = document.createElement('span');
+            subItemElem.classList.add('preview-item-val');
+            subItemElem.innerHTML = `${listItem[key]}`;
+            itemElem.appendChild(subItemElem);
+        }
+
+        listContainer.appendChild(itemElem);
+    })
+}
+
+const displayCV = (userData) => {
+    nameDsp.innerHTML = userData.firstname + " " + userData.middlename + " " + userData.lastname;
+    phonenoDsp.innerHTML = userData.phoneno;
+    emailDsp.innerHTML = userData.email;
+    addressDsp.innerHTML = userData.address;
+    linkedinDsp.innerHTML = userData.linkedin;
+    githubDsp.innerHTML = userData.github;
+    captionDsp.innerHTML = userData.caption;
+    showListData(userData.skills, skillsDsp);
+    showListData(userData.certificates, certificatesDsp);
+    showListData(userData.languages, languagesDsp);
+    showListData(userData.interests, interestsDsp);
+    showListData(userData.educations, educationsDsp);
+    showListData(userData.experiences, experiencesDsp);
+}
+
+// generate CV
+const generateCV = () => {
+    let userData = getUserInputs();
+    displayCV(userData);
+    console.log(userData);
+}
+
+function previewImage(){
+    let oFReader = new FileReader();
+    oFReader.readAsDataURL(imageElem.files[0]);
+    oFReader.onload = function(ofEvent){
+        imageDsp.src = ofEvent.target.result;
+    }
+}
+
+// print CV
+function printCV(){
+    window.print();
+}
